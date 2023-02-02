a02efce22a9b65a7f607eb726b8458bafaf629d1
milkcoke <mbh023@gmail.com>
2023-02-02 17:51:44 +0900

    docs: Update README.md
    Add module, controller, service, pipe , dto, vo description in Nest.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { BoardModule } from './board/board.module';
 import './utils/bigintHelpers';
 
 @Module({
+  // root 모듈 (AppModule) 이 부모
+  // imports 대상 모듈이 자식 또는 참조 관계가 된다.
   imports: [BoardModule],
   controllers: [AppController],
   providers: [AppService],
