dc7fcc65726a1e9c5005dd398102c6c6cd790e7b
milkcoke <mbh023@gmail.com>
2023-02-02 14:24:33 +0900

    feat: Register controller and provider in board module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardModule } from './board/board.module';
+import './utils/bigintHelpers';
 
 @Module({
-  imports: [],
+  imports: [BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
