545edc8f01496152a037973756fc198a14c6d61b
milkcoke <mbh023@gmail.com>
2023-02-03 12:53:11 +0900

    build: Apply default configuration file using config package

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {Logger} from "@nestjs/common";
+import * as config from 'config';
+
+const serverConfig  = config.get('server');
 
 async function bootstrap() {
   const logger = new Logger();
   // application 생성
   // 모든 모듈의 root module 이 AppModule
   const app = await NestFactory.create(AppModule);
-  const PORT = 3000;
+  const PORT = serverConfig['port'];
 
   await app.listen(PORT)
       .then(()=>logger.log(`Application running on port: ` + PORT));
